3577d6baae224e4d1ba2877825d4e3d8750a90cf
Henna Petroff <nettikakadu@gmail.com>
2015-08-27 12:33:27 +0300

    Luotu sekvenssikaaviot

--- Laivanupotus/Laivanupotus/src/laivanupotus/Aloitusruutu.java
+++ Laivanupotus/Laivanupotus/src/laivanupotus/Aloitusruutu.java
@@ -1,17 +1,24 @@
-
 package laivanupotus;
+
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
-
+import java.awt.TextArea;
+import java.awt.TextComponent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Aloitusruutu extends JPanel{
     
     public Aloitusruutu(final JFrame frame){
+        this.setLayout(new BorderLayout());
         JButton nappula = new JButton("Lol toimiiko xD");
+        JTextArea tekstialue = new JTextArea(1,20);
+        tekstialue.setEditable(true);        
         nappula.addActionListener(new ActionListener() {
 
             @Override
@@ -20,7 +27,11 @@ public class Aloitusruutu extends JPanel{
             }
             
         });
-        this.add(nappula);
+        
+        JPanel panel = new JPanel();
+        panel.add(nappula);
+        panel.add(tekstialue);
+        this.add(panel, BorderLayout.SOUTH);
         
     }
     
